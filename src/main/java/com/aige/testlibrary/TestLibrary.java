97f86f3f6a2409d259f09b8d403e84d6dc9b8e9b
cangck <625963170@qq.com>
2017-07-20 14:50:39 +0800

    初始化项目

---
+++ src/main/java/com/aige/testlibrary/TestLibrary.java
@@ -0,0 +1,13 @@
+package com.aige.testlibrary;
+
+/**
+ * Created by cangck on 17/7/20.
+ */
+
+public class TestLibrary {
+
+    public String getString(){
+        return "hellow jar";
+    }
+
+}
